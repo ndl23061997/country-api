6b28aca2340525dc0ff338f54752df94e1c608e2
Nguyen Duc Long <longnguyen23061997@gmail.com>
2019-08-27 21:53:51 +0700

    Long enable cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require("cors");
 var routes = require("./routes");
 
 var app = express();
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", routes);
 
